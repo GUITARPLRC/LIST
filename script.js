b787cf0add4eecedaade513d00490147b553e390
Charles <GUITARPLRC@YAHOO.COM>
2016-03-22 13:29:29 -0500

    add item ok, delete item not ok

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 (function(){
 	
+	var add = document.getElementById("button");
+	var userText = document.getElementById("text");
+	var list = document.getElementById("list");
+	var items = document.getElementsByTagName("li");
+	
+	add.addEventListener("click", addItem, false);
+	userText.addEventListener("submit", addItem, false);
+	list.addEventListener("dblclick", removeItem(this), false);
 	
 	
+	function addItem() {
+		
+		var entry = document.createElement("li");
+		entry.appendChild(document.createTextNode(userText.value));
+		list.appendChild(entry);
+		
+		userText.value = "";
+		userText.focus();
+	}
 	
-	
+	function removeItem(obj) {
+		
+		obj.remove();
+		
+	}
 	
 	
 })();
