57e07bad8d95356d144965d1ab637148d63414c0
Charles <GUITARPLRC@YAHOO.COM>
2016-04-16 14:00:03 -0500

    added check if input field is empty

--- script.js
+++ script.js
@@ -4,21 +4,36 @@
 	var userText = document.getElementById("text");
 	var list = document.getElementById("list");
 	var items = document.getElementsByTagName("li");
+	var warn = document.getElementById("warn");
 	
 	add.addEventListener("click", addItem, false);
 	userText.addEventListener("submit", addItem, false);
 	list.addEventListener("dblclick", removeItem, false);
 	
+	userText.addEventListener("keypress", function(e) {
+		if (e === 13) {
+			addItem();
+		}
+	}, false);
+	
 	userText.focus();
 	
 	function addItem() {
-		
-		var entry = document.createElement("li");
-		entry.appendChild(document.createTextNode(userText.value));
-		list.appendChild(entry);
-		
-		userText.value = "";
-		userText.focus();
+		if (userText.value === "") {
+			
+			userText.className = "warning";
+			warn.style.visibility = "visible";
+			
+		} else {
+			var entry = document.createElement("li");
+			entry.appendChild(document.createTextNode(userText.value));
+			list.appendChild(entry);
+			
+			userText.value = "";
+			userText.focus();
+			userText.className = "";
+			warn.style.visibility = "hidden";
+		}
 	}
 	
 	function getTarget(e) {
