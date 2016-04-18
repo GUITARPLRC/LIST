cb939d157961781774d33f355b8e3dbffcaace78
Charles <GUITARPLRC@YAHOO.COM>
2016-04-18 13:37:47 -0500

    added drag n drop
    ISSUES WITH MULTIPLE ITEMS GETTING BORDER ON OVER AND NULL CONTENT OF
    ADDED ITEMS

--- script.js
+++ script.js
@@ -3,12 +3,24 @@
 	var add = document.getElementById("button");
 	var userText = document.getElementById("text");
 	var list = document.getElementById("list");
-	var items = document.getElementsByTagName("li");
+	var items = document.querySelectorAll("li");
 	var warn = document.getElementById("warn");
+	var dragSrc = null;
 	
 	add.addEventListener("click", addItem, false);
 	userText.addEventListener("submit", addItem, false);
 	list.addEventListener("dblclick", removeItem, false);
+	
+	[].forEach.call(items, function(i){
+		
+		i.addEventListener("dragstart", start, false);
+		i.addEventListener("dragenter", enter, false);
+		i.addEventListener("dragover", over, false);
+		i.addEventListener("gradleave", leave, false);
+		i.addEventListener("drop", drop, false);
+		i.addEventListener("dragend", end, false);
+		
+	});
 	
 	userText.addEventListener("keypress", function(e) {
 		if (e.keyCode === 13) {
@@ -27,6 +39,7 @@
 		} else {
 			var entry = document.createElement("li");
 			entry.appendChild(document.createTextNode(userText.value));
+			entry.setAttribute("draggable", "true");
 			list.appendChild(entry);
 			
 			userText.value = "";
@@ -48,6 +61,63 @@
 		list.removeChild(target);
 		
 		userText.focus();
+		
+	}
+	
+	function start(i) {
+		
+		dragSrc = this;
+		i.dataTransfer.effectAllowed = "move";
+		i.dataTransfer.setData("text/html", this.innerHTML);
+		
+	}
+	
+	function over(i) {
+		
+		if (i.preventDefault) {
+			i.preventDefault();
+		}
+		
+		i.dataTransfer.dropEffect = "move";
+		
+		return false;
+		
+	}
+	
+	function enter(i) {
+		
+		this.classList.add("over");
+		
+	}
+	
+	function leave(i) {
+		
+		this.classList.remove("over");
+		
+	}
+	
+	function drop(i) {
+		
+		if(i.stopPropagation) {
+			i.stopPropagation();
+		}
+		
+		if (dragSrc != this) {
+			dragSrc.innerHTML = this.innerHTML;
+			this.innerHTML = i.dataTransfer.getData("text/html");
+		}
+		
+		return false;
+		
+	}
+	
+	function end(i) {
+		
+		[].forEach.call(items, function(e){
+			
+			e.classList.remove("over");
+			
+		});
 		
 	}
 	
